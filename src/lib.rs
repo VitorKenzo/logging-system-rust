37d12d186801fff14f0fe86cad1872c761678d94
Vitor Pellegatti <vkenzofp@gmail.com>
2023-08-23 21:05:03 -0300

    function to write a single object in a log file implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,41 @@
-pub fn dummy_function() {
-    println!("Hello there!");
+//! In this library we pretend to create a crate to be able to log different objects into a file
+//! Ultimately we are going to have two big functionalities: when to write and one to read from a given file
+
+use std::{fs, io::{Write, BufWriter, BufReader}};
+
+use serde::{Serialize, Deserialize};
+
+
+/// # Function to write into the log
+/// We are going to assume that we must write into a file one specefic object per time
+/// TODO: turn a paramater a generic type deserializable, so that we deserielize it here and write it on the file
+pub fn write_data_into_file<T: Serialize>(path: &str, data: T) -> std::io::Result<()> {
+
+    let file = fs::OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&path)?;
+
+    let mut file = BufWriter::new(file);
+
+    let data_string = serde_json::to_string(&data);
+
+    //trying to write everything in one go
+    file.write_all(&data_string.unwrap().as_bytes())?;
+
+    file.flush()?;
+
+    Ok(())
+}
+
+
+/// # Function to retrieve an especific amount of objects from given log path
+/// this function to be modular enough for us to set the amount of objects we want to retreive
+/// ideally we should be able to give a default good value to n if nothing is passed
+/// TODO: see if its actually possible to retrieve n objects only
+/// TODO: see what the function should return
+/// TODO: a first version to just return the iterator
+pub fn retrieve_from_log_n(path: &str, n: Option<u32>) {//-> serde_json::StreamDeserializer<'_, serde_json::de::IoRead<BufReader<File>>, serde_json::Value> {
+    println!("{} {}", path, n.unwrap_or(4));
+    println!("TODO");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,38 @@
-use logging_system::dummy_function;
+use logging_system::{write_data_into_file, retrieve_from_log_n};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+struct DummyObjects {
+    a: u32,
+    b: u32
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Dummy {
+    id: u32,
+    comment: String,
+    objects: Vec<DummyObjects>,
+}
 
 fn main() {
-    println!("Hello, world!");
-    dummy_function();
+    
+    let objects = vec![
+        DummyObjects { a: 12, b: 10},
+        DummyObjects { a: 8, b: 9},
+        DummyObjects { a: 7, b: 8},
+    ];
+
+    let dummy = Dummy {
+        id: 8,
+        comment: "test".to_string(),
+        objects: objects,
+    };
+
+    //using the writing function
+    match write_data_into_file("test1.log", &dummy) {
+        Ok(_) => println!("Succeded in writing in the file"),
+        Err(e) => println!("Something went wrong: {e}"),
+    };
+
+    retrieve_from_log_n(&"teste".to_string(), Some(5));
 }
