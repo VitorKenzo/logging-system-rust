2feb8851fc3cc3fc82f66420ce1654dd732ce40d
Vitor Pellegatti <vkenzofp@gmail.com>
2023-08-30 16:06:04 -0300

    1st version of retrieve function + organizing comments and stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
 //! In this library we pretend to create a crate to be able to log different objects into a file
 //! Ultimately we are going to have two big functionalities: when to write and one to read from a given file
 
-use std::{fs, io::{Write, BufWriter, BufReader}};
+use std::{fs, fs::File, io::{Write, BufWriter, BufReader}};
+use serde::Serialize;
+use serde_json::{Deserializer, Value, Error};
 
-use serde::{Serialize, Deserialize};
-
-
-/// # Function to write into the log
-/// We are going to assume that we must write into a file one specefic object per time
-/// TODO: turn a paramater a generic type deserializable, so that we deserielize it here and write it on the file
+/// Write the data object passed into the specified file
+/// 
+/// This function takes as paramater a Serializable data and properly writes it into a file
+/// that is specified in the path paramater.
+/// 
+/// In case the file does not exist it automatically creates it and then writes in it, and in case it does exist,
+/// it appends the current object in the file.
+/// 
+///  # Panics
+/// 
+/// This function will panic in any case of an IO error
 pub fn write_data_into_file<T: Serialize>(path: &str, data: T) -> std::io::Result<()> {
 
     let file = fs::OpenOptions::new()
@@ -20,22 +27,38 @@ pub fn write_data_into_file<T: Serialize>(path: &str, data: T) -> std::io::Resul
 
     let data_string = serde_json::to_string(&data);
 
-    //trying to write everything in one go
     file.write_all(&data_string.unwrap().as_bytes())?;
-
     file.flush()?;
 
     Ok(())
+    
 }
 
 
-/// # Function to retrieve an especific amount of objects from given log path
-/// this function to be modular enough for us to set the amount of objects we want to retreive
-/// ideally we should be able to give a default good value to n if nothing is passed
+/// Function to retrieve all objects from a file
+/// 
+/// This function takes as a paramater a path to a file that holds JSON objects and then retrieves
+/// all the objects and return them inside an iterator to the function caller.
+/// 
+/// Apperantly the function is robust enough to even be able to read an empty file and return with no problems, but so far
+/// we don't really know how many objecst are retrieved from the file
+/// 
+///  # Panics
+/// 
+/// This function panics in case the file does not exist.
+/// 
 /// TODO: see if its actually possible to retrieve n objects only
-/// TODO: see what the function should return
-/// TODO: a first version to just return the iterator
-pub fn retrieve_from_log_n(path: &str, n: Option<u32>) {//-> serde_json::StreamDeserializer<'_, serde_json::de::IoRead<BufReader<File>>, serde_json::Value> {
-    println!("{} {}", path, n.unwrap_or(4));
-    println!("TODO");
+pub fn retrieve_from_log_n(path: &str) -> Result<impl Iterator<Item = Result<Value, Error>>, std::io::Error> {
+
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // this can be a one liner
+    let deserilizer = Deserializer::from_reader(reader);
+    // the type of the below variable is serde_json::StreamDeserializer<'_, serde_json::de::IoRead<BufReader<File>>, Value>
+    // more information here https://docs.rs/serde_json/latest/serde_json/struct.StreamDeserializer.html
+    let iterator = deserilizer.into_iter::<Value>();
+    
+    Ok(iterator)
+
 }
--- src/main.rs
+++ src/main.rs
@@ -16,23 +16,38 @@ struct Dummy {
 
 fn main() {
     
+    // creating generic objects to test the functions
     let objects = vec![
         DummyObjects { a: 12, b: 10},
         DummyObjects { a: 8, b: 9},
         DummyObjects { a: 7, b: 8},
     ];
-
     let dummy = Dummy {
         id: 8,
         comment: "test".to_string(),
         objects: objects,
     };
 
-    //using the writing function
+    // Using the writing function
     match write_data_into_file("test1.log", &dummy) {
         Ok(_) => println!("Succeded in writing in the file"),
         Err(e) => println!("Something went wrong: {e}"),
     };
 
-    retrieve_from_log_n(&"teste".to_string(), Some(5));
+    // Using the retrieving function
+    let objects = retrieve_from_log_n(&"test1.log".to_string()).unwrap();
+
+    let mut dummies: Vec<Dummy> = Vec::new();
+    for item in objects {
+        let json_item = item.unwrap();
+        //println!("Got {:#?}", json_item);
+        
+        dummies.push(serde_json::from_value::<Dummy>(json_item).unwrap())
+    }
+
+    // printing the retrieved objects
+    for dummy in dummies {
+        println!("Dummy found! {:#?}", dummy);
+    }
+
 }
